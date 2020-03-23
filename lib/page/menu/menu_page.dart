0f93b06cf6fa18287cd10fc8fc0de9331ed5e1cb
陈涛 <chentao@triumen.com>
2020-03-23 23:26:12 +0800

    完善菜单页UI

--- lib/page/menu/menu_page.dart
+++ lib/page/menu/menu_page.dart
@@ -1,9 +1,64 @@
 import 'package:flutter/material.dart';
 
 class MenuPage extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return Text("菜单");
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            icon: Icon(Icons.clear),
+            onPressed: () {
+
+            }
+        ),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+
+              }
+              )
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(
+                "单读",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 13
+              ),
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                  child: Divider(
+                    color: Colors.white30,
+                    height: 1,
+                  )
+              ),
+              Text(
+                  "WE READ THE WORLD",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15
+                ),
+              ),
+              Expanded(
+                  child: Divider(
+                    color: Colors.white30,
+                    height: 1,
+                  )
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 
 }
